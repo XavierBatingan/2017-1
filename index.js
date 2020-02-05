a72f7e032f02e6bf27417900e0c648af632c837a
Xavier Batingan <xavierbatingan@seattleacademy.org>
2020-02-05 09:14:47 -0800

    Add counter to server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+const express = require('express')
+const app = express()
+const port = 1509
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello Xavier!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send("counter " + counter);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
